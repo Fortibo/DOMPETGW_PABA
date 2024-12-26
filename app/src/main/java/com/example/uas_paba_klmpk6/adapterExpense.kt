6896c334a85ccefd6f73c8e34fd3cf5643ddd008
Dohazo <pootisspy931@gmail.com>
2024-12-26 11:46:14 +0700

    rework on adapter and main

--- app/src/main/java/com/example/uas_paba_klmpk6/adapterExpense.kt
+++ app/src/main/java/com/example/uas_paba_klmpk6/adapterExpense.kt
@@ -9,6 +9,8 @@ import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.example.uas_paba_klmpk6.database.expense
 import com.example.uas_paba_klmpk6.database.income
+import java.text.NumberFormat
+import java.util.Locale
 
 class adapterExpense (private val expenseData : MutableList<expense>)  : RecyclerView.Adapter<adapterExpense.ListViewHolder> () {
     private lateinit var onItemClickCallback : OnItemClickCallback
@@ -42,7 +44,13 @@ class adapterExpense (private val expenseData : MutableList<expense>)  : Recycle
         holder.tvExpenseType.setText(expense.category)
         holder.tvExpenseTitle.setText(expense.title)
         holder.tvExpenseDate.setText(expense.date)
-        holder.tvExpenseMoney.setText(expense.amount)
+
+        val rupiahFormat: NumberFormat = NumberFormat.getCurrencyInstance(Locale("id", "ID"))
+        rupiahFormat.setMaximumFractionDigits(0) // Menghilangkan desimal
+        val formattedAmount: String = rupiahFormat.format(expense.amount)
+        val moneyExpense = "+$formattedAmount"
+
+        holder.tvExpenseMoney.setText(moneyExpense)
 
 
     }
