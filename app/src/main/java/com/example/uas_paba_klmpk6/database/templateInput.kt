3d8410081d5f74cdf7d6a217f68ed75f002fc547
Misael Yosa <107090127+misaelyosa@users.noreply.github.com>
2024-12-27 10:12:40 +0700

    create : db table Template

---
+++ app/src/main/java/com/example/uas_paba_klmpk6/database/templateInput.kt
@@ -0,0 +1,30 @@
+package com.example.uas_paba_klmpk6.database
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class templateInput(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id_template")
+    var id : Int = 0,
+
+    @ColumnInfo(name = "type")
+    var type: String,
+
+    @ColumnInfo(name = "amount")
+    var amount: Int = 0,
+
+    @ColumnInfo(name = "category")
+    var category: String,
+
+    @ColumnInfo(name = "title")
+    var title: String,
+
+    @ColumnInfo(name = "note")
+    var note: String? = null,
+
+    @ColumnInfo(name = "date")
+    val date: String
+)
