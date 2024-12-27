0679db95e3536aaf74e543d99947bb245328fd5e
Misael Yosa <107090127+misaelyosa@users.noreply.github.com>
2024-12-27 11:07:56 +0700

    template recycler

---
+++ app/src/main/java/com/example/uas_paba_klmpk6/adapterTemplate.kt
@@ -0,0 +1,56 @@
+package com.example.uas_paba_klmpk6
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.uas_paba_klmpk6.database.templateInput
+import java.text.NumberFormat
+import java.util.Locale
+
+class adapterTemplate(private val templateData: MutableList<templateInput>) : RecyclerView.Adapter<adapterTemplate.ListViewHolder> () {
+
+    class ListViewHolder(itemView: View):RecyclerView.ViewHolder(itemView) {
+        var _tvTemplateName = itemView.findViewById<TextView>(R.id.tvTemplateName)
+        var _tvTemplateType = itemView.findViewById<TextView>(R.id.tvTemplateType)
+        var _tvTemplateAmount = itemView.findViewById<TextView>(R.id.tvTemplateAmount)
+        var _tvTemplateNote = itemView.findViewById<TextView>(R.id.tvTemplateNote)
+        val _tvTemplateDate = itemView.findViewById<TextView>(R.id.tvTemplateDate)
+        var _tvTemplateCategory = itemView.findViewById<TextView>(R.id.tvTemplateCategory)
+
+        var _tvRemind = itemView.findViewById<TextView>(R.id.tvRemind)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
+        val view: View = LayoutInflater.from(parent.context).inflate(
+            R.layout.item_template_recycler, parent, false
+        )
+        return ListViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return templateData.size
+    }
+    fun isiTemplateData(newTemplateData: MutableList<templateInput>){
+        templateData.clear()
+        templateData.addAll(newTemplateData)
+        notifyDataSetChanged()
+    }
+
+    override fun onBindViewHolder(holder: adapterTemplate.ListViewHolder, position: Int) {
+        var template = templateData[position]
+        val rupiahFormat = NumberFormat.getCurrencyInstance(Locale("in", "ID"))
+        rupiahFormat.setMaximumFractionDigits(0)
+
+        holder._tvTemplateName.setText(template.title)
+        holder._tvTemplateType.setText(template.type)
+        holder._tvTemplateAmount.text = rupiahFormat.format(template.amount)
+        holder._tvTemplateNote.setText(template.note)
+        holder._tvTemplateDate.setText(template.date)
+        holder._tvTemplateCategory.setText(template.category)
+        holder._tvRemind.setText("30 days")
+    }
+
+
+}
