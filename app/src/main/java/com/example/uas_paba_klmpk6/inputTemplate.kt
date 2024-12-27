d083867cd27333a4eee8dd5358d30eb3c1977ba3
Misael Yosa <107090127+misaelyosa@users.noreply.github.com>
2024-12-27 10:22:18 +0700

    init DatePicker

--- app/src/main/java/com/example/uas_paba_klmpk6/inputTemplate.kt
+++ app/src/main/java/com/example/uas_paba_klmpk6/inputTemplate.kt
@@ -1,12 +1,20 @@
 package com.example.uas_paba_klmpk6
 
+import android.app.DatePickerDialog
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.uas_paba_klmpk6.database.mainDB
+import java.util.Calendar
 
 class inputTemplate : AppCompatActivity() {
+    private lateinit var datePickerDialog: DatePickerDialog
+    private lateinit var DB:mainDB
+    private var selectedDate:String ?= null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +24,41 @@ class inputTemplate : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        DB = mainDB.getDatabase(this)
+    }
+
+
+//    DATEPICKER
+    private fun initDatePicker(){
+        val dateListener = DatePickerDialog.OnDateSetListener { _, year, month, day ->
+            val date = makeDateString(day, month + 1, year)
+            var _tvSelected = findViewById<TextView>(R.id.selectedDate)
+            _tvSelected.setText(date)
+            selectedDate = date
+        }
+
+        val cal = Calendar.getInstance()
+        val year = cal.get(Calendar.YEAR)
+        val month = cal.get(Calendar.MONTH)
+        val day = cal.get(Calendar.DAY_OF_MONTH)
+
+        datePickerDialog = DatePickerDialog(
+            this,
+            dateListener,
+            year,
+            month,
+            day
+        )
+    }
+
+    private fun makeDateString(day: Int, month: Int, year: Int): String {
+        val formattedDay = if (day < 10) "0$day" else "$day"
+        val formattedMonth = if (month < 10) "0$month" else "$month"
+        return "$year-$formattedMonth-$formattedDay"
+    }
+
+    private fun openDatePicker(){
+        datePickerDialog.show()
     }
 }
