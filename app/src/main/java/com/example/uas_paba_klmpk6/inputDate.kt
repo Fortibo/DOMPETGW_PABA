a66a394b854d81cbf0d7025938a09716b57ad49d
FarrellYunanto <farrellyunanto@gmail.com>
2024-12-27 14:09:27 +0700

    fix date

--- app/src/main/java/com/example/uas_paba_klmpk6/inputDate.kt
+++ app/src/main/java/com/example/uas_paba_klmpk6/inputDate.kt
@@ -23,6 +23,8 @@ import java.util.Date
 
 class inputDate : AppCompatActivity() {
     private lateinit var datePickerDialog: DatePickerDialog
+    private var selectedDate:String ?= null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -83,6 +85,7 @@ class inputDate : AppCompatActivity() {
             val date = makeDateString(day, month + 1, year)
             var _tvSelected = findViewById<TextView>(R.id.selectedDate)
             _tvSelected.setText(date)
+            selectedDate = date
         }
 
         val cal = Calendar.getInstance()
@@ -99,26 +102,10 @@ class inputDate : AppCompatActivity() {
         )
     }
 
-    private fun makeDateString(day: Int, month: Int, year: Int): String{
-        return getMonthFormat(month) + " " + day + " " + year
-    }
-
-    private fun getMonthFormat(month: Int): String {
-        return when (month) {
-            1 -> "JAN"
-            2 -> "FEB"
-            3 -> "MAR"
-            4 -> "APR"
-            5 -> "MAY"
-            6 -> "JUN"
-            7 -> "JUL"
-            8 -> "AUG"
-            9 -> "SEP"
-            10 -> "OCT"
-            11 -> "NOV"
-            12 -> "DEC"
-            else -> "INVALID"
-        }
+    private fun makeDateString(day: Int, month: Int, year: Int): String {
+        val formattedDay = if (day < 10) "0$day" else "$day"
+        val formattedMonth = if (month < 10) "0$month" else "$month"
+        return "$year-$formattedMonth-$formattedDay"
     }
 
     private fun openDatePicker(){
